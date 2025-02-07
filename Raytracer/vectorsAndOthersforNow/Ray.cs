dbc1f9ae299edd1897d66ac0523fb67fed8e25e5
MateuszBlazejowski <matblaz12345@gmail.com>
2025-02-07 23:52:50 +0100

    finished, comparing using structs and classes is avaliable

--- Raytracer/vectorsAndOthersforNow/Ray.cs
+++ Raytracer/vectorsAndOthersforNow/Ray.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿// only one can be created at a time, used to check the difference in speed
+
+//#define structs 
+#define classes 
+using System;
 
 namespace Raytracer.vectorsAndOthersforNow
 {
+#if structs
     public struct Ray
     {
         public Point3 Origin { get; }
@@ -28,4 +33,29 @@ namespace Raytracer.vectorsAndOthersforNow
             );
         }
     }
+#endif
+#if classes
+    public class Ray
+    {
+        // only get, should not be changed after being created (for now?)
+        public Point3 Origin { get; }
+        public Vec3 Direction { get; }
+
+        // default
+        public Ray() : this(new Point3(0, 0, 0), new Vec3(0, 0, 0)) { }
+
+        public Ray(Point3 origin, Vec3 direction)
+        {
+            Origin = origin;
+            Direction = direction;
+        }
+
+        // Returns the point at a given parameter t along the ray
+        public Point3 At(double t)
+        {
+            Vec3 result = Origin + t * Direction;
+            return new Point3(result.X, result.Y, result.Z);
+        }
+    }
+#endif
 }
--- Raytracer/vectorsAndOthersforNow/vec3.cs
+++ Raytracer/vectorsAndOthersforNow/vec3.cs
@@ -1,4 +1,8 @@
-﻿global using Point3 = Raytracer.vectorsAndOthersforNow.Vec3;
+﻿// only one can be created at a time, used to check the difference in speed
+//#define structs 
+#define classes 
+
+global using Point3 = Raytracer.vectorsAndOthersforNow.Vec3;
 global using ColorV2 = Raytracer.vectorsAndOthersforNow.Vec3;
 
 using System;
@@ -6,6 +10,7 @@ using System.IO;
 
 namespace Raytracer.vectorsAndOthersforNow
 {
+#if structs
     public struct Vec3
     {
         public double X, Y, Z;
@@ -81,4 +86,96 @@ namespace Raytracer.vectorsAndOthersforNow
             output.WriteLine($"{rByte} {gByte} {bByte}");
         }
     }
+#endif
+#if classes
+     public class Vec3
+ {
+     public double[] E { get; } = new double[3];
+
+     public Vec3() : this(0, 0, 0) { }
+     public Vec3(double e0, double e1, double e2)
+     {
+         E[0] = e0;
+         E[1] = e1;
+         E[2] = e2;
+     }
+
+     public double X => E[0];
+     public double Y => E[1];
+     public double Z => E[2];
+
+     public Vec3 Negate() => new Vec3(-E[0], -E[1], -E[2]);
+
+     public double GetIndex(int i) => E[i];
+     public void SetIndex(int i, double value) => E[i] = value;
+
+     public Vec3 Add(Vec3 v)
+     {
+         return new Vec3(E[0] + v.E[0], E[1] + v.E[1], E[2] + v.E[2]);
+     }
+
+     public Vec3 Subtract(Vec3 v)
+     {
+         return new Vec3(E[0] - v.E[0], E[1] - v.E[1], E[2] - v.E[2]);
+     }
+
+     public Vec3 Multiply(double t)
+     {
+         return new Vec3(E[0] * t, E[1] * t, E[2] * t);
+     }
+
+     public Vec3 Divide(double t)
+     {
+         return Multiply(1 / t);
+     }
+
+     public double Length() => Math.Sqrt(LengthSquared());
+     public double LengthSquared() => E[0] * E[0] + E[1] * E[1] + E[2] * E[2];
+
+     public static Vec3 operator +(Vec3 u, Vec3 v) => u.Add(v);
+     public static Vec3 operator -(Vec3 u, Vec3 v) => u.Subtract(v);
+     public static Vec3 operator *(Vec3 v, double t) => v.Multiply(t);
+     public static Vec3 operator *(double t, Vec3 v) => v.Multiply(t);
+     public static Vec3 operator /(Vec3 v, double t) => v.Divide(t);
+     public static Vec3 operator -(Vec3 v) => v.Negate();
+
+     // Dot Product
+     public static double Dot(Vec3 u, Vec3 v)
+     {
+         return u.E[0] * v.E[0] + u.E[1] * v.E[1] + u.E[2] * v.E[2];
+     }
+
+     // Cross Product
+     public static Vec3 Cross(Vec3 u, Vec3 v)
+     {
+         return new Vec3(
+             u.E[1] * v.E[2] - u.E[2] * v.E[1],
+             u.E[2] * v.E[0] - u.E[0] * v.E[2],
+             u.E[0] * v.E[1] - u.E[1] * v.E[0]
+         );
+     }
+
+     // Unit Vector
+     public static Vec3 UnitVector(Vec3 v) => v / v.Length();
+
+     public override string ToString() => $"{E[0]} {E[1]} {E[2]}";
+
+
+     //for the color application of Vec3
+     public void WriteColor(TextWriter output, ColorV2 pixelColor)
+     {
+         // Extract RGB values
+         double ray = pixelColor.X;
+         double g = pixelColor.Y;
+         double b = pixelColor.Z;
+
+         // Convert from [0,1] to [0,255] and ensure valid range
+         int rByte = (int)(255.999 * Math.Clamp(ray, 0, 1));
+         int gByte = (int)(255.999 * Math.Clamp(g, 0, 1));
+         int bByte = (int)(255.999 * Math.Clamp(b, 0, 1));
+
+         output.WriteLine($"{rByte} {gByte} {bByte}");
+     }
+ }
+#endif
 }
